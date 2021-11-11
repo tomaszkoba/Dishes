555941f0c0fc640dfc8219f58ae6bc01351dc10f
Tomasz Koba <tomaszkoba@outlook.com>
2021-11-11 16:05:39 +0100

    Initial Commit

---
+++ Dishes/DishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishesApp.swift
+//  Dishes
+//
+//  Created by Tomasz Koba on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct DishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
